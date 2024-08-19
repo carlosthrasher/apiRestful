6bfd32b109f0931e1ddaafc5fb9251ce5bb78c68
carlosthrasher <carloshcpereira@outlook.com.br>
2024-08-19 19:33:35 -0300

    Base do projeto para aprendizado

---
+++ src/test/java/dio/apiRestful/ApiRestfulApplicationTests.java
@@ -0,0 +1,13 @@
+package dio.apiRestful;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiRestfulApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
